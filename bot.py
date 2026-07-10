d71ae6b324380af8d9a45d0ec6fa8f128379ca63
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-10 20:26:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-print("Hello, Telegram Bot!")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = 8892992589:AAFfx5dwCizTvMOEe_uKJSE2y1a9qqrXPBQ
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! Your Telegram trading bot is now running.")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
