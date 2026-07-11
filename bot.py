b1a64b2317e3a27ca353e406dcc59323d13d14e6
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-11 12:29:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,16 @@ TOKEN = "8892992589:AAHJaYwKpyNj3Kp7-oYnreE3WEvdvxEkGKw"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Your Telegram bot is working.")
-  async def analysis(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+
+async def analysis(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("EUR/USD Analysis is coming soon.")
+
 
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("analysis", analysis))
     print("Bot is running...")
     app.run_polling()
 
