014cb10c4bc16668b41770713ec9921d8df9ba4a
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-14 14:15:35 +0100

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,10 +1,33 @@
 import asyncio
 
+from strategy import analyze_market
 
-async def start_scanner():
+
+CHAT_ID = "6588451803"
+
+
+async def start_scanner(app):
+
+    last_signal = None
+
     while True:
         print("Scanner running... checking market")
 
-        # Strategy check will be added here later
+        result = analyze_market()
+
+        # Send only BUY or SELL signals
+        if "BUY 🟢" in result or "SELL 🔴" in result:
+
+            # Prevent duplicate messages
+            if result != last_signal:
+
+                await app.bot.send_message(
+                    chat_id=CHAT_ID,
+                    text=result
+                )
+
+                last_signal = result
+
+                print("Trade signal sent to Telegram")
 
         await asyncio.sleep(300)
